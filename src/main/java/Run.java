b0df619536a264bb7b315d4e5e06e962e0a0d254
vasyl_okhrimenko <v.okhrimenko@gmail.com>
2021-05-04 11:13:40 +0300

    Create Run class

---
+++ src/main/java/Run.java
@@ -0,0 +1,11 @@
+public class Run {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Загадай число от 1 до 100");
+        Thread.sleep(5000);
+        System.out.println("Загадали?");
+        Thread.sleep(1200);
+        System.out.println("Давай угадаю!");
+    }
+
+}
